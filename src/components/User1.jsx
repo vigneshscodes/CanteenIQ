f9b3450e164014766d516bf1609942005d1cf8b1
vigneshscodes <vigneshs2224@gmail.com>
2026-06-09 10:38:15 +0530

    fix-time-seconds

--- src/components/User1.jsx
+++ src/components/User1.jsx
@@ -109,7 +109,7 @@ export default function User1() {
         status: "Pending",
         counterno: Math.floor(Math.random() * 5) + 1,
         tokenno: tokenCounter++,
-        expectedDelvtime: new Date(Date.now() + 10 * 60000).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }),
+        expectedDelvtime: new Date(Date.now() + 10 * 60000),
         otp: Math.floor(1000 + Math.random() * 9000).toString(),
       };
 
